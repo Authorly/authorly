3ef3189a23f530ddae9d133396dfc1835ca3ce39
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-03-01 20:43:33 +0530

    Use RAILS_ENV instead of RAILS_ENV.

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 require 'active_record'
 
-ENV['RAILS_ENV'] ||= 'development'
+ENV['RACK_ENV'] ||= 'production'
 
-config = YAML::load(ERB.new(IO.read(File.join(File.dirname(__FILE__), '..', 'db', 'config.yml'))).result)[ENV['RAILS_ENV']].symbolize_keys
+config = YAML::load(ERB.new(IO.read(File.join(File.dirname(__FILE__), '..', 'db', 'config.yml'))).result)[ENV['RACK_ENV']].symbolize_keys
 ActiveRecord::Base.establish_connection(config)
 
 class User < ActiveRecord::Base
