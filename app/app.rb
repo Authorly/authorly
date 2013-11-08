f39a0a164af067807b34efc4ffd98120c17faba9
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-11-08 22:19:13 +0530

    (#28) Make sure User has_secure_password

--- app/app.rb
+++ app/app.rb
@@ -17,6 +17,8 @@ class MailerQueue
 end
 
 class User < ActiveRecord::Base
+  has_secure_password
+
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates_uniqueness_of :email, :case_sensitive => false, :message => 'is in use.'
   validates_presence_of :email
