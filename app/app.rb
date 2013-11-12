9f535599bd766bdee8f696f1bd5ced20e4eaec2a
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-11-12 14:54:46 +0530

    (#34) Remove mailchimp integration.

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 require 'json'
 require 'active_record'
-require 'mailchimp'
 require 'resque'
 
 ENV['RACK_ENV'] ||= 'production'
@@ -23,18 +22,8 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email, :case_sensitive => false, :message => 'is in use.'
   validates_presence_of :email
 
-  after_create :add_to_mailchimp
   before_create { generate_token(:confirmation_token) }
   before_create { generate_token(:auth_token) }
-
-  @mailchimp_api = nil
-
-  def self.mailchimp_api
-    return @mailchimp_api if @mailchimp_api
-    api_key = YAML::load(ERB.new(IO.read(File.join(File.dirname(__FILE__), '..', 'config', 'mailchimp.yml'))).result)[ENV['RACK_ENV']].symbolize_keys[:api_key]
-
-    @mailchimp_api = Mailchimp::API.new(api_key)
-  end
 
   def save_with_password
     pass = SecureRandom.base64(32).gsub(/[=+$\/]/, '')[0..8]
@@ -48,34 +37,6 @@ class User < ActiveRecord::Base
     begin
       self[column] = SecureRandom.urlsafe_base64
     end while User.exists?(column => self[column])
-  end
-
-  private
-
-  def add_to_mailchimp
-    info = {
-      :id            => 'dae12d02db', # Mailchimp list id for "Authorly New User Signup" list
-      :email_address => self.email,
-      :double_optin  => false,
-      :send_welcome  => false,
-    }
-
-    #if self.promo.to_s.match(/\A\d\d\d\w\w\w\Z/) # If promo code matches 'XXXYYY' Where X is a letter and Y is a digit
-      #info[:merge_vars] = { 'PCODE' => self.promo.to_s }
-    #end
-
-    # Check out documentation for listSubscribe:
-    # http://apidocs.mailchimp.com/api/1.3/listsubscribe.func.php
-    begin
-      self.class.mailchimp_api.listSubscribe(info)
-      File.open(File.join(File.dirname(__FILE__), '..', 'log', ENV['RACK_ENV'] + '.log'), 'a') do |logger|
-        logger.puts("Welcome notification email sent to: #{self.email}\n")
-      end
-    rescue => e
-      File.open(File.join(File.dirname(__FILE__), '..', 'log', ENV['RACK_ENV'] + '.log'), 'a') do |logger|
-        logger.puts("Could not send welcome notification email to: #{self.email}\n")
-      end
-    end
   end
 end
 
