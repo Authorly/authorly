91b46440666890597c04e744d35d1917688cf02d
Chris Whitman <christopherscottwhitman@gmail.com>
2013-12-23 11:48:36 -0800

    Yet another fix for #47 - properly parse GET param from URL

--- app/public/js/functions.js
+++ app/public/js/functions.js
@@ -297,6 +297,7 @@ $(".books li").click(function(){
 });
 
 $.urlParam = function(name){
-    var results = new RegExp('[\\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
-    return results[1] || 0;
+  return decodeURI(
+      (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
 }
