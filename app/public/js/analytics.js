3474b7d0b922f679c820f8421ba0ad1d5597aad0
Chris Whitman <christopherscottwhitman@gmail.com>
2014-01-04 17:25:48 -0800

    Move Google Analytics to external Javascript file with KISS metrics

--- app/public/js/analytics.js
+++ app/public/js/analytics.js
@@ -1,4 +1,16 @@
+// Google Analytics
+ var _gaq = _gaq || [];
+ _gaq.push(['_setAccount', 'UA-38933484-1']);
+ _gaq.push(['_trackPageview']);
 
+ (function() {
+   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ })();
+
+
+// KISS metrics
 var _kmq = _kmq || [];
 var _kmk = _kmk || 'eaa44bcbccb5653fdfe62261d025ec89ee4e6802';
 function _kms(u){
@@ -11,4 +23,3 @@ function _kms(u){
 }
 _kms('//i.kissmetrics.com/i.js');
 _kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
-
