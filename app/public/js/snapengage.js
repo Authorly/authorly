8902dc2d923186e97118ffb9281eef3892337b53
Chris Whitman <christopherscottwhitman@gmail.com>
2014-01-05 09:04:50 -0800

    #59 SnapEgage live chat

---
+++ app/public/js/snapengage.js
@@ -0,0 +1,5 @@
+(function() {
+  var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+  se.src = '//commondatastorage.googleapis.com/code.snapengage.com/js/7bb262a8-693c-4241-8708-f8c2ef781364.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+})();
